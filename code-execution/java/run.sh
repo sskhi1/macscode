958d88d1aa042cfc4f3fe67c1c124ac1f1b0e7c9
Koba Karaputadze <106602441+zkenshinx@users.noreply.github.com>
2024-03-31 19:23:07 +0400

    Refactor docker test results (#13)

--- code-execution/java/run.sh
+++ code-execution/java/run.sh
@@ -4,11 +4,11 @@
 cd execution
 
 mkdir result
-echo ${test_count} > result/result.txt
+echo ${test_count} > result/test_count.txt
 
 javac src/*.java
 
 for test_num in $(seq 1 "${test_count}")
 do
-  java -classpath src/ Main tests/in_"${test_num}".txt tests/out_"${test_num}".txt result/result.txt
+  java -classpath src/ Main tests/in_"${test_num}".txt tests/out_"${test_num}".txt result/result_"${test_num}".txt
 done
--- executor/src/main/java/freeuni/macs/macscode/service/ExecutionResultsExtractor.java
+++ executor/src/main/java/freeuni/macs/macscode/service/ExecutionResultsExtractor.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,18 +13,21 @@ import java.util.List;
 @Service
 public class ExecutionResultsExtractor {
 
+    private String readOneLineFromFile(String filePath) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+        return reader.readLine();
+    }
 
     public List<SingleTestCaseResult> getResultsFromExecutionDir(Path executionDir) {
-        String resultsFilePath = String.format("%s/result/result.txt", executionDir.toFile().getAbsolutePath());
+        String prefixPath = executionDir.toFile().getAbsolutePath() + "/result";
         List<SingleTestCaseResult> allTestCaseResults = new ArrayList<>();
         try {
-            BufferedReader resultsReader = new BufferedReader(new FileReader(resultsFilePath));
-            int testCasesCount = Integer.parseInt(resultsReader.readLine());
+            int testCasesCount = Integer.parseInt(readOneLineFromFile(String.format("%s/test_count.txt", prefixPath)));
             for (int i = 0; i < testCasesCount; ++i) {
-                String result = resultsReader.readLine();
+                String result = readOneLineFromFile(String.format("%s/result_%d.txt", prefixPath, i + 1));
                 allTestCaseResults.add(new SingleTestCaseResult(i, result));
             }
-        } catch (Exception e) {
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
         return allTestCaseResults;
