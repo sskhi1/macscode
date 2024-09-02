9690098bc55b58d3cade6946320c9d974cc91e95
Koba Karaputadze <kobakaraputadze701@gmail.com>
2024-09-02 19:37:25 +0400

    Add Admin Adu

---
+++ auth-service/src/main/resources/db/migration/V0.0.3__create_tables.sql
@@ -0,0 +1,2 @@
+INSERT INTO macscode_schema.users (name, username, email, password, role)
+VALUES ('Admin', 'Admin', 'Admin@gmail.com', '$2a$10$njLJhzCxUz6BqFl0LVbwueUTwxLgNg5DZntHY.EypeyIkQhMHCzK.', 'ADMIN');
