6d45d1484322228571a4c6e19210e7780ac07133
aadua20 <aadua20@freeuni.edu.ge>
2024-09-02 21:20:29 +0400

    show topics header

--- macscode-ui/src/components/Profile.js
+++ macscode-ui/src/components/Profile.js
@@ -210,7 +210,7 @@ const Profile = () => {
                             className="count">{abstractionsCount}</span> {abstractionsCount === 1 ? 'Problem Solved' : 'Problems Solved'}
                         </p>
                         {
-                            topicsCount.length > 0 &&
+                            Object.keys(topicsCount).length > 0 &&
                             <h4>Topics</h4>
                         }
                         <div className="topics-container">
